f0a5be4ad67399300fa02653fbfecf40e14e087d
reedyrm <richard.g.reedy@gmail.com>
2018-08-08 20:57:35 -0500

    not working without react-navigation

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StackNav } from './StackNav';
+import { StyleSheet, Text, Animated } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <StackNav />
+      <Animated.View>
+        <Text>Something</Text>
+      </Animated.View>
     );
   }
 }
